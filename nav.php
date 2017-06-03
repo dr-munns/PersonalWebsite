3d6f9a470396652ff037eb11bb66ff573789557e
dr Munns <kaspermunns@gmail.com>
2017-06-03 14:49:32 +0100

    test

--- nav.php
+++ nav.php
@@ -1,10 +1,6 @@
 <?php
   function NavBar() {
 
-    return '<ul>
-          <li><a href="index.htm">Home</a></li>
-          <li><a href="new.htm">News</a></li>
-          <li><a href="old.htm">Contact</a></li>
-        </ul>';
+    return 'test';
   }
  ?>
