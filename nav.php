60c0bee14a4263b217a3039b27a80d655f253fe0
dr Munns <kaspermunns@gmail.com>
2017-06-03 14:59:52 +0100

    tweak

--- nav.php
+++ nav.php
@@ -2,10 +2,9 @@
   function NavBar() {
     return
       '<ul>
-        <li><a href="default.asp">Home</a></li>
+        <li><a href="index.php">Home</a></li>
         <li><a href="news.asp">News</a></li>
         <li><a href="contact.asp">Contact</a></li>
-        <li><a href="about.asp">About</a></li>
       </ul>';
   }
  ?>
