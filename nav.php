d74635ce0221e958c1e8aa59721b1467a5bf3115
dr Munns <kaspermunns@gmail.com>
2017-06-03 14:48:05 +0100

    move navbar to php
    1st test

--- nav.php
+++ nav.php
@@ -1,6 +1,10 @@
 <?php
   function NavBar() {
 
-    return "Nul";
+    return "<ul>
+          <li><a href="index.htm">Home</a></li>
+          <li><a href="new.htm">News</a></li>
+          <li><a href="old.htm">Contact</a></li>
+        </ul>";
   }
  ?>
