a9de0bb3bfd392c531a8dc5ff97d3d371a31e522
dr Munns <kaspermunns@gmail.com>
2017-06-03 15:17:35 +0100

    just hard code for now
    all back to html

--- index.php
+++ index.php
@@ -20,10 +20,11 @@
 
   </body>
 
-  <div>
-    <?php
-
-    ?>
-  </div>
+  <ul>
+    <li><a class="active" href="index.php">Home</a></li>
+    <li><a href="#news">News</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#about">About</a></li>
+  </ul>
 
 </html>
