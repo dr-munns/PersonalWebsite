0e6ef5ba8f5c1d3c6a3cc20a736e947aa64d8275
dr Munns <kaspermunns@gmail.com>
2017-06-03 14:58:45 +0100

    back to navbar

--- nav.php
+++ nav.php
@@ -1,6 +1,11 @@
 <?php
   function NavBar() {
-
-    return 'test';
+    return
+      '<ul>
+        <li><a href="default.asp">Home</a></li>
+        <li><a href="news.asp">News</a></li>
+        <li><a href="contact.asp">Contact</a></li>
+        <li><a href="about.asp">About</a></li>
+      </ul>';
   }
  ?>
