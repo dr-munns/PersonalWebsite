0dee1fe728e8330359df66829feb55c22334156e
dr Munns <kaspermunns@gmail.com>
2017-06-03 14:49:02 +0100

    typo
    quote unquote

--- nav.php
+++ nav.php
@@ -1,10 +1,10 @@
 <?php
   function NavBar() {
 
-    return "<ul>
+    return '<ul>
           <li><a href="index.htm">Home</a></li>
           <li><a href="new.htm">News</a></li>
           <li><a href="old.htm">Contact</a></li>
-        </ul>";
+        </ul>';
   }
  ?>
