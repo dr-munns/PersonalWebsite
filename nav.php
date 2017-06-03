2b05ab012d833a8e6f703fd172b0ea3894607f99
dr Munns <kaspermunns@gmail.com>
2017-06-03 15:00:41 +0100

    hello?

--- nav.php
+++ nav.php
@@ -1,10 +1,6 @@
 <?php
   function NavBar() {
     return
-      '<ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="news.asp">News</a></li>
-        <li><a href="contact.asp">Contact</a></li>
-      </ul>';
+      'WHATCHA';
   }
  ?>
